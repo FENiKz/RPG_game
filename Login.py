6499e037f91984546417aa10d84df13a65da17f3
Tomislav Perisic <tomiperisic@gmail.com>
2019-07-19 21:55:35 +0200

    Added comments to the code

--- Login.py
+++ Login.py
@@ -1,17 +1,21 @@
-#This module check if an account with the provided username exsists and check for password if it exists
+#This module checks if an account with the provided username exsists and checks if the provided password is correct
 
 import os
 
+#Beautification variable
 Field_Separator = "\n\n------------------------------------------------------------------------------------------\n\n"
 
 def login():
     Username = input(Field_Separator + "Username: ")
+
     Profile_data = {}
+
     if os.path.exists('./' + Username + '.txt'):
         with open(Username + ".txt") as f:
             for line in f:
                 (key, val) = line.split()
                 Profile_data[(key)] = val
+
             Login_Attempts = 3
             while Login_Attempts > 0:
                 password = input("Password: ")
